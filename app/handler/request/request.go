93a9e640190c29f77863c79ea319b32213b329b8
jono-taiga <jono-taiga@dmm.com>
2022-02-21 19:57:23 +0900

    remove pkg/errors since archived

--- app/handler/request/request.go
+++ app/handler/request/request.go
@@ -1,11 +1,11 @@
 package request
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/go-chi/chi"
-	"github.com/pkg/errors"
 )
 
 // Read path parameter `id`
@@ -13,12 +13,12 @@ func IDOf(r *http.Request) (int64, error) {
 	ids := chi.URLParam(r, "id")
 
 	if ids == "" {
-		return -1, errors.Errorf("id was not presence")
+		return -1, fmt.Errorf("id was not presence")
 	}
 
 	id, err := strconv.ParseInt(ids, 10, 64)
 	if err != nil {
-		return -1, errors.Errorf("id was not number")
+		return -1, fmt.Errorf("id was not number")
 	}
 
 	return id, nil
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/go-chi/cors v1.1.1
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jmoiron/sqlx v1.3.4
-	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
 )
