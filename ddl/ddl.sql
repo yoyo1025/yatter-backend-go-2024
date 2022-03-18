6132249247a7d9c82fbde4aecc58b82712440d4d
jono-taiga <jono-taiga@dmm.com>
2022-03-18 16:18:04 +0900

    drop status impl

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -9,13 +9,3 @@ CREATE TABLE `account` (
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
-
-CREATE TABLE `status` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `account_id` bigint(20) NOT NULL,
-  `content` text NOT NULL,
-  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  INDEX `idx_account_id` (`account_id`),
-  CONSTRAINT `fk_status_account_id` FOREIGN KEY (`account_id`) REFERENCES  `account` (`id`)
-);
