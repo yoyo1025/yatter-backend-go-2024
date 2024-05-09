050de7214633dd007aad6047f6095090fa99a651
Yuta <snys.yutayuta@gmail.com>
2024-05-09 18:59:09 +0900

    fix: レビューへの対応

--- app/handler/statuses/create.go
+++ app/handler/statuses/create.go
@@ -1,21 +1,22 @@
 package statuses
 
 import (
+	"fmt"
 	"net/http"
+	"yatter-backend-go/app/handler/auth"
 )
 
 // Request body for `POST /v1/statuses`
 type AddRequest struct {
 	Status string
-	// TODO: Medias Field
 }
 
 // Handle request for `POST /v1/statuses`
 func (h *handler) Create(w http.ResponseWriter, r *http.Request) {
 	//ctx := r.Context()
 
-	// account_info := auth.AccountOf(r) 認証情報を取得する
+	account_info := auth.AccountOf(r) // 認証情報を取得する
 
-	panic("Must Implement Status Creation")
+	panic(fmt.Sprintf("Must Implement Status Creation And Acount Info %v", account_info))
 
 }
