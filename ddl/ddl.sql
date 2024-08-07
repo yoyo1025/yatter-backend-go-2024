f91e7e0b3e07e96fbc091c765d6525f82ec474da
yoyo1025 <hsjejxys@gmail.com>
2024-08-07 14:01:43 +0900

    statusテーブルを作成

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -9,3 +9,12 @@ CREATE TABLE `account` (
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `status` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `account_id` BIGINT(20) NOT NULL,
+  `content` VARCHAR(255),
+  `create_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`account_id`) REFERENCES `account`(`id`) ON DELETE CASCADE
+);
