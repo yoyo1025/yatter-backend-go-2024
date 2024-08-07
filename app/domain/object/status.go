d18599aa493d137a2def362ba7e271631a811413
yoyo1025 <hsjejxys@gmail.com>
2024-08-07 14:34:12 +0900

    StatusDetail構造体を定義

--- app/domain/object/status.go
+++ app/domain/object/status.go
@@ -10,6 +10,14 @@ type Status struct {
 	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at"`
 }
 
+type StatusDetail struct {
+	ID               int `json:"id"`
+	Account          Account
+	Content          string    `json:"status"`
+	CreatedAt        time.Time `json:"created_at,omitempty"`
+	MediaAttachments Attachment
+}
+
 func NewStatus(content string) *Status {
 	return &Status{
 		Content:   content,
